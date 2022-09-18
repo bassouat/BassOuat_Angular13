49ded19f99abe5f08b2c7a82913689c868f1b794
Basseydou Ouattara <bassouat8@gmail.com>
2022-09-18 16:12:49 +0200

    Add an hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -33,6 +33,14 @@ export class HeroService {
       catchError(this.handleError<any>('updateHero'))
     );
   }
+
+  /** POST: add a new hero to the server */
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
   getHero(id: number): Observable<Hero> {
     // For now, assume that a hero with the specified `id` always exists.
     // Error handling will be added in the next step of the tutorial.
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -17,7 +17,14 @@ export class HeroesComponent implements OnInit {
   getHeroes():void {
     this.heroService.getHeroes().subscribe(mockheroes => this.mockheroes = mockheroes)
   }
-
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.mockheroes.push(hero);
+      });
+  }
   ngOnInit(): void {
     this.getHeroes();
   }
